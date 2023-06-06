d85c4474279e576732151287d99d51b639aa569c
Ankhaa <ankhaaaae@gmail.com>
2023-06-05 20:40:31 -0500

    spruce up

--- lambda.py
+++ lambda.py
@@ -1,54 +1,105 @@
+from bs4 import BeautifulSoup
 import json
-
 import re
 import requests
 
-from bs4 import BeautifulSoup
+# regex pattern to get title of next wikipedia page
+wiki_link_pattern = r"/wiki/(.*)"
 
-pattern = r"/wiki/(.*)"
+def get_next_page(paragraphs):
+    """
+    Helper function for the lambda do parse the paragraphs of a wikipedia page
+    to find the first link to another wikipedia page, not counting first links
+    in parentheses.
 
+    Parameters
+    ----------
+    elements: (list('bs4.element.Tag'))
+        Expected to be paragraphs of a wikipedia page. 
 
-            
-    
-def get_next_page(elements):
-    # Elements is list of `p` elements
-    
-    for p in elements:
-        # Stringify the element to detect it
-        p = str(p.encode().decode())
+    Returns
+    ------
+    page_title: (string)
+        The first string 
+    """
 
-        last_five_chars = ''
-        page_title = ''
-        building_url = False
-        opend = False
+    for p in paragraphs:
+        #convert to string
+        p = p.prettify()
+
+        page_title = ""
+        next_page  = ""
+        last_six_chars = ""
+        parsing_link = False
+
+        in_paren = False
         parens = 0
-        for c in p:
-            last_five_chars = (last_five_chars + c)[-6:]
-            if building_url and not opend:
-                if c == '"':
-                    match = re.search(pattern, page_title)
-                        # Extract the captured group (text after "/wiki/")
+
+        for char in p:
+            last_six_chars = (last_six_chars + char)[-6:]
+            
+            if parsing_link and not in_paren:
+                if char == '"':
+                # Signifies end of link
+                    match = re.search(wiki_link_pattern, page_title)
                     if match:
-                        extracted_text = match.group(1)
-                        page_title = extracted_text
-                        print(page_title)
+                        next_page = match.group(1)
+                        print(next_page)
                         break
                 else:
-                    page_title += c
-            elif c == '(':
-                parens +=1
-                opend = True
-            elif c == ')' and opend:
+                    page_title += char
+
+            elif char == '(':
+                parens += 1
+                in_paren = True
+
+            elif char == ')' and in_paren:
                 parens -= 1
-                if parens== 0:
-                    opend=False
+                if parens == 0:
+                    in_paren = False
                     page_title = ""
-            if last_five_chars == 'href="' and not opend:
-                building_url = True
-        if page_title:
-            return page_title
+
+            if last_six_chars == 'href="' and not in_paren:
+                parsing_link = True
+        
+        # If a page title has been found, return
+        # Otherwise check next paragraph
+        if next_page: return next_page
 
 def lambda_handler(event, context):
+    """
+    This function extracts the path parameter from the event, and follows a 
+    chain of Wikipedia links starting from the given page until it reaches the
+    "Philosophy" page or encounters a loop. It returns a list of pages visited 
+    in the chain, or a message indicating that the original page does not lead 
+    to Philosophy.
+
+    Parameters
+    ----------
+    event: (dict) 
+        The event object containing information about the event triggering the Lambda function.
+        
+        pathParameters: (dict)
+            A dictionary containing path parameters.
+        page: (str): 
+            The name of the URL parameter specifying the starting page.
+
+    context (LambdaContext):
+        The context object representing the runtime information of the Lambda function.
+
+    Returns
+    ------
+    response (dict):
+        Response to be turned into json to respond to server.
+
+        statusCode: 200/400
+            OK or Bad Request
+        
+        body: list(string)  or string
+            List of pages that take the path or message indicating a loop when
+            trying to find path or if an error took place.
+    """
+
     try:
         # Extract path parameters
         path_parameters = event['pathParameters']
@@ -56,7 +107,7 @@ def lambda_handler(event, context):
         # Assuming 'page' is the name of your URL parameter
         page = original_page = path_parameters['page']
         
-        pages = []
+        pages = [page]
 
         while page and page != "Philosophy" and page not in pages:
             pages.append(page)
@@ -64,9 +115,9 @@ def lambda_handler(event, context):
             url = f'https://en.wikipedia.org/wiki/{page}'
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'html.parser')
-            elements = soup.select('div.mw-parser-output p')
+            paragraphs = soup.select('div.mw-parser-output p')
             
-            page = get_next_page(elements)
+            page = get_next_page(paragraphs)
         
         if page == 'Philosophy':
             pages.append(page)
--- wikipedia.py
+++ wikipedia.py
@@ -2,56 +2,134 @@ import requests
 from bs4 import BeautifulSoup
 import re 
 
+wiki_link_pattern = r"/wiki/(.*)"
 
+def get_next_page(paragraphs):
+    """
+    Helper function for the lambda do parse the paragraphs of a wikipedia page
+    to find the first link to another wikipedia page, not counting first links
+    in parentheses.
 
-URL = "https://en.wikipedia.org/wiki/Telecommunications"
-pattern = r"/wiki/(.*)"
+    Parameters
+    ----------
+    elements: list('bs4.element.Tag')
+        Expected to be paragraph objects with 
 
-page = "Albert_Einstein"
-print(page)
-while page != "Philosophy":
-    URL = "https://en.wikipedia.org/wiki/" + page
-    response = requests.get(URL)
+    Returns
+    ------
+    page_title: string
+        The first string 
 
-    # Create a BeautifulSoup object from the response content
-    soup = BeautifulSoup(response.content, 'html.parser')
+    """
 
-    # Find the first anchor tag (link) on the page
+    for p in paragraphs:
+        #convert to string
+        p = p.prettify()
 
-    div = soup.find('div', id='mw-content-text').find('div', class_='mw-parser-output')
-    if div:
-        paragraphs = soup.find_all('p')
-        for p in paragraphs:
-            p = str(p.encode().decode())
-            last_five_chars = ''
-            url = ''
-            building_url = False
-            opend = False
-            parens = 0
-            for c in p:
-                last_five_chars = (last_five_chars + c)[-6:]
-                if building_url and not opend:
-                    if c == '"':
-                        match = re.search(pattern, url)
-                         # Extract the captured group (text after "/wiki/")
-                        if match:
-                            extracted_text = match.group(1)
-                            page = extracted_text
-                            print(page)
-                            break
-                    else:
-                        url += c
-                elif c == '(':
-                    parens +=1
-                    opend = True
-                elif c == ')' and opend:
-                    parens -= 1
-                    if parens== 0:
-                        opend=False
-                        url = ""
-                if last_five_chars == 'href="' and not opend:
-                    building_url = True
+        page_title = ""
+        next_page  = ""
+        last_six_chars = ""
+        parsing_link = False
+
+        # To ignore parentheses
+        in_paren = False
+        parens = 0
+
+        for char in p:
+            last_six_chars = (last_six_chars + char)[-6:]
             
-            if url: break
+            if parsing_link and not in_paren:
+                if char == '"':
+                    # Signifies end of link
+                    match = re.search(wiki_link_pattern, page_title)
+                    if match:
+                        next_page = match.group(1)
+                        print(next_page)
+                        break
+                else:
+                    page_title += char
+
+            elif char == '(':
+                parens += 1
+                in_paren = True
+
+            elif char == ')' and in_paren:
+                parens -= 1
+                if parens== 0:
+                    in_paren=False
+                    page_title = ""
+
+            if last_six_chars == 'href="' and not in_paren:
+                parsing_link = True
+        
+        # If a page title has been found, return
+        # Otherwise check next paragraph
+        if next_page: return next_page
+pages = []
+page = original_page = "Penis"
+while page and page != "Philosophy" and page not in pages:
+    pages.append(page)
+    
+    url = f'https://en.wikipedia.org/wiki/{page}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    paragraphs = soup.select('div.mw-parser-output p')
+    
+    page = get_next_page(paragraphs)
+
+if page == 'Philosophy':
+    pages.append(page)
+
+# URL = "https://en.wikipedia.org/wiki/Telecommunications"
+# pattern = r"/wiki/(.*)"
+
+# page = "Quadrupedalism"
+# print(page)
+# while page != "Philosophy":
+#     URL = "https://en.wikipedia.org/wiki/" + page
+#     response = requests.get(URL)
+
+#     # Create a BeautifulSoup object from the response content
+#     soup = BeautifulSoup(response.content, 'html.parser')
+
+#     # Find the first anchor tag (link) on the page
+
+#     div = soup.find('div', id='mw-content-text').find('div', class_='mw-parser-output')
+#     if div:
+#         paragraphs = soup.find_all('p')
+#         for p in paragraphs:
+#             print(type(p))
+
+#             p = p.prettify()
+#             last_five_chars = ''
+#             url = ''
+#             building_url = False
+#             opend = False
+#             parens = 0
+#             for c in p:
+#                 last_five_chars = (last_five_chars + c)[-6:]
+#                 if building_url and not opend:
+#                     if c == '"':
+#                         match = re.search(pattern, url)
+#                          # Extract the captured group (text after "/wiki/")
+#                         if match:
+#                             extracted_text = match.group(1)
+#                             page = extracted_text
+#                             print(page)
+#                             break
+#                     else:
+#                         url += c
+#                 elif c == '(':
+#                     parens +=1
+#                     opend = True
+#                 elif c == ')' and opend:
+#                     parens -= 1
+#                     if parens== 0:
+#                         opend=False
+#                         url = ""
+#                 if last_five_chars == 'href="' and not opend:
+#                     building_url = True
+            
+#             if url: break
 
             
